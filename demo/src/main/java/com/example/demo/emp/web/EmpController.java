9829ec5dc9ba069d8c92e731af5d96b32194236f
'uk' <'dnrdl6817@gamil.com'>
2023-04-21 17:54:46 +0900

    가즈아아

---
+++ demo/src/main/java/com/example/demo/emp/web/EmpController.java
@@ -0,0 +1,33 @@
+package com.example.demo.emp.web;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.emp.mapper.EmpMapper;
+
+@Controller
+public class EmpController {
+	
+	@Autowired
+	EmpMapper emp;
+	
+	@GetMapping("/")
+	@ResponseBody
+	public String main() {
+		return "hello";
+	}
+	
+	
+	//사원 전체 조회
+	@GetMapping("/getEmpList")
+	public String getEmpList(Model model){
+		model.addAttribute("empList", emp.selectEmpAllList()  );
+		return "empList";
+	}
+	
+	
+}
